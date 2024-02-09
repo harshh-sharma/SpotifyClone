fa704e14a1c3c9c17517139ec36de73af8b6da8d
harshh-sharma <harsh877sharma@gmail.com>
2024-02-09 16:30:39 +0530

    Add music functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let currentSongs = new Audio();
+
 async function getSongs(){
     const response = await fetch('http://127.0.0.1:5500/songs/')
     const data = await response.text();
@@ -16,11 +18,36 @@ async function getSongs(){
 
 async function main(){
     const songs = await getSongs();
-    console.log(songs);
-    
-    var audio = new Audio(songs[0]);
-    audio.play();
+    const ul = document.querySelector('.songNames');
+    for( i = 0 ; i < songs.length ; i++){
+        let name = (songs[i].replace('http://127.0.0.1:5500/songs/','')).replace('_320(PaglaSongs).mp3','');
+        ul.innerHTML = ul.innerHTML + `
+        <li>
+                    <img class="invert" src="./assets/music-player.png"/>
+                    <div class="info">
+                        <div>${name}</div>
+                    </div>
+                    <div class="playnow">
+                        <img class="invert" src="./assets/play-button.png" alt="">
+                    </div>
+                </li>
+        `
+    } 
 
+    const playMusic = (track) => {
+        console.log(track);
+        // const audio = new Audio();
+        currentSongs.src = ('/songs/' + track + '_320(PaglaSongs).mp3')
+        currentSongs.play();
+    }
+
+    Array.from(document.querySelector('.songs-list').getElementsByTagName('li')).forEach(e => {
+        e.addEventListener('click',element =>{
+            console.log(e.querySelector('.info').firstElementChild.innerHTML);
+            playMusic(e.querySelector('.info').firstElementChild.innerHTML);
+        })
+       
+    });
 }
 
 main();
